331cc3e84ce73fab459c7669e9a714c4870b2fe6
OlesiaSub <lesya.sub@mail.ru>
2020-03-11 16:21:45 +0300

    add algorithm

---
+++ algo/cmake-build-debug/CMakeFiles/hse_project_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hse_project_algorithm.dir/src/main.cpp.obj"
+  "CMakeFiles/hse_project_algorithm.dir/src/path.cpp.obj"
+  "hse_project_algorithm.exe"
+  "hse_project_algorithm.exe.manifest"
+  "hse_project_algorithm.pdb"
+  "libhse_project_algorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hse_project_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

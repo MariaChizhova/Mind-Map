e886ae402dbb46eb9e8cc17a01f45e168c39b834
Maria <machizhova@edu.hse.ru>
2020-04-30 22:58:09 +0300

    Add main window

---
+++ include/MainWindow.h
@@ -0,0 +1,28 @@
+#ifndef PROJECT_HSE_MAINWINDOW_H
+#define PROJECT_HSE_MAINWINDOW_H
+
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QApplication>
+#include <menu.h>
+#include "MainWindow.h"
+
+
+class MainWindow: public QMainWindow {
+public:
+    MainWindow(QWidget *parent = 0);
+
+    ~MainWindow() = default;
+
+private slots:
+    void newFileButton();
+
+    void openFileButton();
+
+    void newProject();
+
+    Menu *window;
+
+};
+#endif //PROJECT_HSE_MAINWINDOW_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-#include "menu.h"
+#include <QtWidgets/QApplication>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    Menu window;
-    window.setStyleSheet("background-color: #13011E; color: white");
+    MainWindow window;
+    window.setStyleSheet("color: white; background-image: url(:/icons/123.jpg)");
     window.resize(1920, 1080);
     window.setWindowTitle("Mind Map");
-    window.scene.setBackgroundBrush(Qt::white);
     window.show();
     return app.exec();
 }

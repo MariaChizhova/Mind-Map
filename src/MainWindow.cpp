aeae83383389a8e62891cfcf96979bb57baf1bef
Maria <machizhova@edu.hse.ru>
2020-05-01 16:39:30 +0300

    Change sizes

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -4,14 +4,16 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     screenSize = QGuiApplication::primaryScreen()->physicalSize();
     qreal w2 = screenSize.width() * 2.5;
     qreal h2 = screenSize.height() * 2;
+    qreal wr = 200;
+    qreal hr = 80;
     QPushButton *newFile = new QPushButton("New File", this);
-    newFile->setGeometry(w2, h2, 200, 80);
+    newFile->setGeometry(w2, h2, wr, hr);
     connect(newFile, &QPushButton::clicked, this, &MainWindow::newFileButton);
     QPushButton *openFile = new QPushButton("Open File", this);
-    openFile->setGeometry(w2, h2 + 80 , 200, 80);
+    openFile->setGeometry(w2, h2 + hr , wr, hr);
     connect(openFile, &QPushButton::clicked, this, &MainWindow::openFileButton);
     QPushButton *quitBtn = new QPushButton("Quit", this);
-    quitBtn->setGeometry(w2, h2 + 160, 200, 80);
+    quitBtn->setGeometry(w2, h2 + 2 * hr, wr, hr);
     connect(quitBtn, &QPushButton::clicked, this, QApplication::quit);
 }
 
